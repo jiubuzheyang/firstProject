4e7062bd6075ede418a0c1056c09b52ff1d38a37
luoluosuifeng <912025885@qq.com>
2021-09-04 14:31:41 +0800

    第一次创建项目

---
+++ src/main/java/com/study/myfirst/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.study.myfirst.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/say")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello, my java project";
+    }
+
+}
