445823cf71708eb0825814e9b7a90853fb70e5e7
liu <912025885@qq.com>
2025-02-03 18:57:37 +0800

    添加注释

--- src/main/java/com/study/myfirst/controller/HelloController.java
+++ src/main/java/com/study/myfirst/controller/HelloController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author 91202
+ */
 @RestController
 @RequestMapping("/say")
 public class HelloController {
